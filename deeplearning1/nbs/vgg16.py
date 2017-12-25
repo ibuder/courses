c55bf880af85116e3c162c1eecf73907613dff2a
Immanuel Buder <ibuder@gmail.com>
2017-12-24 22:21:05 -0800

    More Keras1->Keras2 updates

--- deeplearning1/nbs/vgg16.py
+++ deeplearning1/nbs/vgg16.py
@@ -1,3 +1,5 @@
+# See http://forums.fast.ai/t/keras-2-released/1956/42
+# for updates for Keras2
 from __future__ import division, print_function
 
 import os, json
@@ -11,7 +13,7 @@ from keras.layers.normalization import BatchNormalization
 from keras.utils.data_utils import get_file
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout, Lambda
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.layers.pooling import GlobalAveragePooling2D
 from keras.optimizers import SGD, RMSprop, Adam
 from keras.preprocessing import image
@@ -97,7 +99,7 @@ class Vgg16():
         model = self.model
         for i in range(layers):
             model.add(ZeroPadding2D((1, 1)))
-            model.add(Convolution2D(filters, 3, 3, activation='relu'))
+            model.add(Conv2D(filters, (3, 3), activation='relu'))
         model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
 
@@ -174,7 +176,7 @@ class Vgg16():
                 batches : A keras.preprocessing.image.ImageDataGenerator object.
                           See definition for get_batches().
         """
-        self.ft(batches.nb_class)
+        self.ft(batches.num_classes)
         classes = list(iter(batches.class_indices)) # get a list of all the class labels
         
         # batches.class_indices is a dict with the class name as key and an index as value
@@ -209,8 +211,10 @@ class Vgg16():
             Fits the model on data yielded batch-by-batch by a Python generator.
             See Keras documentation: https://keras.io/models/model/
         """
-        self.model.fit_generator(batches, samples_per_epoch=batches.nb_sample, nb_epoch=nb_epoch,
-                validation_data=val_batches, nb_val_samples=val_batches.nb_sample)
+        self.model.fit_generator(batches, steps_per_epoch=max(batches.samples//batches.batch_size, 1), 
+                                 epochs=nb_epoch,
+                                 validation_data=val_batches,
+                                 validation_steps=max(val_batches.samples//val_batches.batch_size, 1))
 
 
     def test(self, path, batch_size=8):
@@ -227,5 +231,5 @@ class Vgg16():
     
         """
         test_batches = self.get_batches(path, shuffle=False, batch_size=batch_size, class_mode=None)
-        return test_batches, self.model.predict_generator(test_batches, test_batches.nb_sample)
+        return test_batches, self.model.predict_generator(test_batches, max(test_batches.samples//test_batches.batch_size,1))
 
